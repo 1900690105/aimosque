aaa485a290c726a2c30545b62620a93fdfadb561
nikhil kandhare <98297220+1900690105@users.noreply.github.com>
2025-09-15 11:13:14 +0530

    nikhil's 2nd commit

--- frontend/src/components/pages/MyPrayer.jsx
+++ frontend/src/components/pages/MyPrayer.jsx
@@ -51,10 +51,6 @@ export default function MyPrayer() {
                       type="button"
                       role="tab"
                       aria-controls="missed"
-<<<<<<< HEAD:frontend/src/components/pages/MyPrayer.jsx
-                      // data-state="inactive"
-=======
->>>>>>> 216a4b1694ce3709df5d3029a21f045b361623b2:src/components/pages/MyPrayer.jsx
                       id="missed"
                       className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
                       tabindex="-1"
